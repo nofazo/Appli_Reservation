2a242a107db35108bb2bba1ab4bc0668e1fe6e9a
nofazo <nouhi.f.z@gmail.com>
2017-09-26 14:40:47 +0200

    Titre&Prix

---
+++ Reservation.php
@@ -0,0 +1,13 @@
+</!DOCTYPE html>
+<html>
+<head>
+	<title>Réservation</title>
+</head>
+<body>
+<?php
+ echo "Le prix de la place est de 10 euros jusqu'à 12 ans et ensuite de 15 euros"
+ echo "le prix de l'assurance annulation est de 20 euros quel que soit le nombre de voyageur"
+
+?>
+</body>
+</html>
