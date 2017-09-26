facbab8502b4cb0bf01789817d32ec661e76b563
nofazo <nouhi.f.z@gmail.com>
2017-09-26 16:16:47 +0200

    Ajout formulaire

--- Reservation.php
+++ Reservation.php
@@ -1,13 +1,25 @@
-</!DOCTYPE html>
 <html>
-<head>
+	<head>
 	<title>Réservation</title>
-</head>
-<body>
-<?php
- echo "Le prix de la place est de 10 euros jusqu'à 12 ans et ensuite de 15 euros"
- echo "le prix de l'assurance annulation est de 20 euros quel que soit le nombre de voyageur"
-
-?>
-</body>
+	</head>
+	<body>
+	 <h1> Nouvelle réservation </h1>
+ 	 <p>Le prix de la place est de 10 euros jusqu'à 12 ans et ensuite de 15 euros </br> Le prix de l'assurance annulation est de 20 euros quel que soit le nombre de voyageur</p>
+	</body>
+	<form>
+		<fieldset>
+			<legend>Votre recherche</legend>
+			<label>Destination </label>
+				<select>
+					<option> Berlin </option>
+					<option> Bruxelles</option>
+					<option> Marrakech</option>
+					<option> Tanger</option>
+					<option> Londres</option>
+				</select></br>
+			<label>Nombre de places</label>
+				<input type="text" > </br>
+			<label> Assurance annulation</label>
+				<input type="checkbox" ></br>	
+		</fieldset>
 </html>
