18bbb64ecc4bc8faf1b2e23ee9bf5b1dddd69ae4
nofazo <nouhi.f.z@gmail.com>
2017-09-27 15:06:50 +0200

    Etape suivante/lien avec controlleur

--- Reservation.php
+++ Reservation.php
@@ -4,22 +4,26 @@
 	</head>
 	<body>
 	 <h1> Nouvelle réservation </h1>
- 	 <p>Le prix de la place est de 10 euros jusqu'à 12 ans et ensuite de 15 euros </br> Le prix de l'assurance annulation est de 20 euros quel que soit le nombre de voyageur</p>
+ 	 <p>Le prix de la place est de 10 euros jusqu'à 12 ans et ensuite de 15 euros. </br> Le prix de l'assurance annulation est de 20 euros quel que soit le nombre de voyageurs.</p>
 	</body>
-	<form>
+	<!--Formulaire qui va appeler Passenger.php en cliquant sur Etape suivante-->
+	<FORM action ="InfoReservation.php" method="post">
 		<fieldset>
-			<legend>Votre recherche</legend>
-			<label>Destination </label>
-				<select>
-					<option> Berlin </option>
-					<option> Bruxelles</option>
-					<option> Marrakech</option>
-					<option> Tanger</option>
-					<option> Londres</option>
+			<legend>Veuillez saisir les informations</legend>
+			<label>Destination</label>
+				<select name= "Destination">
+					<option value = ""> ----Choisir---- </option>
+					<option value = "Belgique"> Bruxelles </option>
+					<option value = "Maroc"> Marrakech </option>
+					<option value = "Maroc"> Tanger </option>
+					<option value ="Royaume-Uni"> Londres </option>
 				</select></br>
 			<label>Nombre de places</label>
-				<input type="text" > </br>
+				<input type="text" name="name" > </br>
 			<label> Assurance annulation</label>
 				<input type="checkbox" ></br>	
 		</fieldset>
+		<input type="submit" value="Etape suivante">
+		<input type="reset" value="Annuler la réservation">
+	<FORM/>	
 </html>
