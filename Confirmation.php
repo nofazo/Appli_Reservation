f0ac8abf4c98d542680d18f722088059eda1c641
Hannouni Houda <Houda-11.03@hotmail.com>
2017-09-29 18:19:30 +0200

    Ajout page de confirmation

---
+++ Confirmation.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Confirmation</title>
+</head>
+<body>
+	<h1>Confirmation des réservations</h1>
+	<p>Votre demande a bien été enrejistrée. </br>
+	   Merci de bien vouloir verser la somme de 2500 euros sur le compte 000-00000-00
+	</p>
+</body>
+<form>
+	<input type="button"  value="Retour à la page d'accueil" onClick="history.go(-3)"> <!-- history.go pas fiable, revoir autre technique-->
+</form>
+</html>
