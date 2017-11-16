7758cfe84b586942866595610293b8d8f05cede0
nofazo <nouhi.f.z@gmail.com>
2017-11-16 10:31:03 +0100

    Ajout de fonctions

--- Modèle_Classes.php
+++ Modèle_Classes.php
@@ -26,11 +26,45 @@ class Reservation
 	public $assurance;
 
 	
-	function __construct($destination, $place, $assurance)
+	public function __construct($destination, $place, $assurance)
 	{
 		$this -> destination = $destination;
 		$this -> place = $place ;
 		$this -> assurance = $assurance ;
+	}
+	public function setDestination($destination) 
+	{ 
+	   $this->destination = $destination; 
+	}
+
+	public function getDestination() 
+	{ 
+	   return $this->destination; 
+	}
+	public function setPlace($place) 
+	{ 
+	   $this->places = $places; 
+	}
+
+	public function getPlace() 
+	{ 
+	   return $this->places; 
+	}
+	public function getAssurance() 
+	{ 
+		if($this->assurance)
+		{
+			return 'OUI';
+		}
+		else
+		{
+			return 'NON';
+		}
+	}
+	
+	public function setAssurance($assurance) 
+	{
+		$this->assurance = $assurance;
 	}
 }
 ?>
